67fb8cd76b06af44e5716e02fe12babd776736a4
Mytlogos <mytlogos@hotmail.com>
2019-09-20 20:27:40 +0200

    move UserPreferences to preferences package

---
+++ app/src/main/java/com/mytlogos/enterprise/preferences/UserPreferences.java
@@ -0,0 +1,48 @@
+package com.mytlogos.enterprise.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class UserPreferences {
+    private static UserPreferences INSTANCE;
+    private static String LOGGED_STATUS = "user_login_status";
+    private static String LOGGED_USER = "user_login_uuid";
+    private SharedPreferences sharedPreferences;
+
+    private UserPreferences(Context context) {
+        if (INSTANCE != null) {
+            throw new IllegalAccessError("Do not instantiate UserPreferences!");
+        }
+        this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public static UserPreferences getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = new UserPreferences(context);
+        }
+        return INSTANCE;
+    }
+
+    public static boolean getLoggedStatus(Context context) {
+        return UserPreferences.getInstance(context).sharedPreferences.getBoolean(LOGGED_STATUS, false);
+    }
+
+    public static String getLoggedUuid(Context context) {
+        return UserPreferences.getInstance(context).sharedPreferences.getString(LOGGED_USER, null);
+    }
+
+    public static void putLoggedStatus(Context context, boolean loggedIn) {
+        SharedPreferences.Editor editor = UserPreferences.getInstance(context).sharedPreferences.edit();
+        editor.putBoolean(LOGGED_STATUS, loggedIn);
+        editor.apply();
+    }
+
+    public static void putLoggedUuid(Context context, String uuid) {
+        SharedPreferences.Editor editor = UserPreferences.getInstance(context).sharedPreferences.edit();
+        editor.putString(LOGGED_USER, uuid);
+        editor.apply();
+    }
+
+
+}
