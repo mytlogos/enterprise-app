9115c0e61ec05592193c83d0cceaf9403265ba60
Mytlogos <mytlogos@hotmail.de>
2021-06-02 12:07:43 +0200

    refactor: rewrite query with joins

--- app/src/main/java/com/mytlogos/enterprise/background/room/MediumDao.kt
+++ app/src/main/java/com/mytlogos/enterprise/background/room/MediumDao.kt
@@ -13,31 +13,35 @@ interface MediumDao : MultiBaseDao<RoomMedium> {
     fun loaded(): List<Int>
 
     @Query("""
-        SELECT * FROM (
-            SELECT title, RoomMedium.mediumId, author, artist, medium, stateTL, stateOrigin, 
-            countryOfOrigin, languageOfOrigin, lang, series, universe, (
-                SELECT episodeId FROM RoomEpisode 
-                INNER JOIN RoomPart ON RoomPart.partId= RoomEpisode.partId
-                WHERE mediumId=RoomMedium.mediumId
-                ORDER BY RoomEpisode.combiIndex DESC
-                LIMIT 1
-            ) as currentRead, (
-            SELECT RoomEpisode.combiIndex 
+        SELECT
+        title, RoomMedium.mediumId, author, artist, medium, stateTL, stateOrigin, 
+        countryOfOrigin, languageOfOrigin, lang, series, universe,
+        lastUpdated, COALESCE(lastEpisodeIndex, 0) as lastEpisode,
+        COALESCE(currentReadEpisodeIndex, 0) as currentReadEpisode, COALESCE(currentReadId, 0) as currentRead
+        FROM RoomMedium 
+        LEFT JOIN (
+            SELECT MAX(RoomEpisode.combiIndex) as currentReadEpisodeIndex,
+            RoomEpisode.episodeId as currentReadId,
+            RoomPart.mediumId
             FROM RoomEpisode
             INNER JOIN RoomPart ON RoomPart.partId=RoomEpisode.partId
-            WHERE RoomPart.mediumId=RoomMedium.mediumId AND RoomEpisode.progress=1
-            ORDER BY RoomEpisode.combiIndex DESC
-            LIMIT 1) as currentReadEpisode, (
-            SELECT MAX(RoomEpisode.combiIndex) FROM RoomEpisode
+            WHERE RoomEpisode.progress=1
+            GROUP BY RoomPart.mediumId
+        ) as cre 
+        ON cre.mediumId=RoomMedium.mediumId
+        LEFT JOIN (
+            SELECT MAX(RoomEpisode.combiIndex) as lastEpisodeIndex, RoomPart.mediumId FROM RoomEpisode
             INNER JOIN RoomPart ON RoomPart.partId=RoomEpisode.partId
-            WHERE RoomPart.mediumId=RoomMedium.mediumId
-            ) as lastEpisode , (
-            SELECT MAX(RoomRelease.releaseDate) FROM RoomEpisode
+            GROUP BY RoomPart.mediumId
+        ) as le
+        ON le.mediumId=RoomMedium.mediumId
+        LEFT JOIN (
+            SELECT MAX(RoomRelease.releaseDate) as lastUpdated, mediumId FROM RoomEpisode
             INNER JOIN RoomRelease ON RoomEpisode.episodeId=RoomRelease.episodeId
             INNER JOIN RoomPart ON RoomPart.partId=RoomEpisode.partId
-            WHERE RoomPart.mediumId=RoomMedium.mediumId
-            ) as lastUpdated FROM RoomMedium
-        ) as RoomMedium 
+            GROUP BY RoomPart.mediumId
+        ) as lastUpdated
+        ON lastUpdated.mediumId=RoomMedium.mediumId
         WHERE (:title IS NULL OR INSTR(lower(title), :title) > 0) 
         AND (:medium = 0 OR (:medium & medium) > 0) 
         AND (:author IS NULL OR INSTR(lower(author), :author) > 0) 
@@ -55,10 +59,51 @@ interface MediumDao : MultiBaseDao<RoomMedium> {
     """)
     fun getAllAsc(sortValue: Int, title: String?, medium: Int, author: String?, lastUpdate: DateTime?, minCountEpisodes: Int, minCountReadEpisodes: Int): DataSource.Factory<Int, MediumItem>
 
-    @Query("""SELECT * FROM (SELECT title, RoomMedium.mediumId, author, artist, medium, stateTL, stateOrigin, countryOfOrigin, languageOfOrigin, lang, series, universe, (   SELECT episodeId FROM RoomEpisode    INNER JOIN RoomPart ON RoomPart.partId= RoomEpisode.partId    WHERE mediumId=RoomMedium.mediumId    ORDER BY RoomEpisode.combiIndex DESC    LIMIT 1) as currentRead,(    SELECT RoomEpisode.combiIndex 
-    FROM RoomEpisode
-    INNER JOIN RoomPart ON RoomPart.partId=RoomEpisode.partId
-    WHERE RoomPart.mediumId=RoomMedium.mediumId AND RoomEpisode.progress=1    ORDER BY RoomEpisode.combiIndex DESC    LIMIT 1) as currentReadEpisode,(   SELECT MAX((COALESCE(RoomEpisode.totalIndex,0) + COALESCE(RoomEpisode.partialIndex,0))) FROM RoomEpisode    INNER JOIN RoomPart ON RoomPart.partId=RoomEpisode.partId     WHERE RoomPart.mediumId=RoomMedium.mediumId) as lastEpisode , (   SELECT MAX(RoomRelease.releaseDate) FROM RoomEpisode    INNER JOIN RoomRelease ON RoomEpisode.episodeId=RoomRelease.episodeId    INNER JOIN RoomPart ON RoomPart.partId=RoomEpisode.partId     WHERE RoomPart.mediumId=RoomMedium.mediumId) as lastUpdated FROM RoomMedium) as RoomMedium WHERE (:title IS NULL OR INSTR(lower(title), :title) > 0) AND (:medium = 0 OR (:medium & medium) > 0) AND (:author IS NULL OR INSTR(lower(author), :author) > 0) AND (:lastUpdate IS NULL OR datetime(lastUpdated) >= datetime(:lastUpdate)) AND (:minCountEpisodes < 0 OR :minCountEpisodes >= lastEpisode) AND (:minCountReadEpisodes < 0 OR :minCountReadEpisodes >= currentReadEpisode) ORDER BY CASE :sortValue WHEN 2 THEN medium WHEN 3 THEN title WHEN 5 THEN author WHEN 7 THEN lastEpisode WHEN 8 THEN currentReadEpisode WHEN 9 THEN lastUpdated ELSE title END DESC""")
+    @Query("""
+        SELECT
+        title, RoomMedium.mediumId, author, artist, medium, stateTL, stateOrigin, 
+        countryOfOrigin, languageOfOrigin, lang, series, universe,
+        lastUpdated, COALESCE(lastEpisodeIndex, 0) as lastEpisode,
+        COALESCE(currentReadEpisodeIndex, 0) as currentReadEpisode, COALESCE(currentReadId, 0) as currentRead
+        FROM RoomMedium 
+        LEFT JOIN (
+            SELECT MAX(RoomEpisode.combiIndex) as currentReadEpisodeIndex,
+            RoomEpisode.episodeId as currentReadId,
+            RoomPart.mediumId
+            FROM RoomEpisode
+            INNER JOIN RoomPart ON RoomPart.partId=RoomEpisode.partId
+            WHERE RoomEpisode.progress=1
+            GROUP BY RoomPart.mediumId
+        ) as cre 
+        ON cre.mediumId=RoomMedium.mediumId
+        LEFT JOIN (
+            SELECT MAX(RoomEpisode.combiIndex) as lastEpisodeIndex, RoomPart.mediumId FROM RoomEpisode
+            INNER JOIN RoomPart ON RoomPart.partId=RoomEpisode.partId
+            GROUP BY RoomPart.mediumId
+        ) as le
+        ON le.mediumId=RoomMedium.mediumId
+        LEFT JOIN (
+            SELECT MAX(RoomRelease.releaseDate) as lastUpdated, mediumId FROM RoomEpisode
+            INNER JOIN RoomRelease ON RoomEpisode.episodeId=RoomRelease.episodeId
+            INNER JOIN RoomPart ON RoomPart.partId=RoomEpisode.partId
+            GROUP BY RoomPart.mediumId
+        ) as lastUpdated
+        ON lastUpdated.mediumId=RoomMedium.mediumId
+        WHERE (:title IS NULL OR INSTR(lower(title), :title) > 0) 
+        AND (:medium = 0 OR (:medium & medium) > 0) 
+        AND (:author IS NULL OR INSTR(lower(author), :author) > 0) 
+        AND (:lastUpdate IS NULL OR datetime(lastUpdated) >= datetime(:lastUpdate)) 
+        AND (:minCountEpisodes < 0 OR :minCountEpisodes >= lastEpisode) 
+        AND (:minCountReadEpisodes < 0 OR :minCountReadEpisodes >= currentReadEpisode) 
+        ORDER BY CASE :sortValue 
+            WHEN 2 THEN medium
+            WHEN 3 THEN title
+            WHEN 5 THEN author
+            WHEN 7 THEN lastEpisode
+            WHEN 8 THEN currentReadEpisode
+            WHEN 9 THEN lastUpdated 
+            ELSE title END DESC
+    """)
     fun getAllDesc(sortValue: Int, title: String?, medium: Int, author: String?, lastUpdate: DateTime?, minCountEpisodes: Int, minCountReadEpisodes: Int): DataSource.Factory<Int, MediumItem>
 
     @Query("""SELECT title, RoomMedium.mediumId, author, artist, medium, stateTL, stateOrigin, countryOfOrigin, languageOfOrigin, lang, series, universe, (   SELECT episodeId FROM RoomEpisode    INNER JOIN RoomPart ON RoomPart.partId= RoomEpisode.partId    WHERE mediumId=RoomMedium.mediumId    ORDER BY RoomEpisode.combiIndex DESC    LIMIT 1) as currentRead,(    SELECT RoomEpisode.combiIndex 
