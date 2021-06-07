700a6737e1f1330dbd5a3b528c5c3d95e6f6717b
Mytlogos <mytlogos@hotmail.de>
2021-06-07 10:13:08 +0200

    refactor: print more descriptive output

--- app/src/main/java/com/mytlogos/enterprise/background/api/Server.kt
+++ app/src/main/java/com/mytlogos/enterprise/background/api/Server.kt
@@ -1,20 +1,23 @@
 package com.mytlogos.enterprise.background.api
 
-import android.annotation.SuppressLint
 import java.io.IOException
 import java.net.InetSocketAddress
 import java.net.Socket
 
-internal class Server(val ipv4: String, val port: Int, val isLocal: Boolean, val isDevServer: Boolean) {
+internal data class Server(
+    val ipv4: String,
+    val port: Int,
+    val isLocal: Boolean,
+    val isDevServer: Boolean,
+) {
 
     //allow unsafe connections for now in local networks
-    @get:SuppressLint("DefaultLocale")
     val address: String
         get() {
             //allow unsafe connections for now in local networks
-            val format = "http" + (if (isLocal) "" else "s") + "://%s:%d/"
-            return String.format(format, ipv4, port)
+            return "http${if (isLocal) "" else "s"}://$ipv4:$port/"
         }
+
     val isReachable: Boolean
         get() {
             try {
@@ -26,13 +29,6 @@ internal class Server(val ipv4: String, val port: Int, val isLocal: Boolean, val
                 return false
             }
         }
-
-    override fun toString(): String {
-        return "Server{" +
-                "ipv4='" + ipv4 + '\'' +
-                ", port=" + port +
-                '}'
-    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
--- app/src/main/java/com/mytlogos/enterprise/background/api/ServerDiscovery.kt
+++ app/src/main/java/com/mytlogos/enterprise/background/api/ServerDiscovery.kt
@@ -18,21 +18,36 @@ import java.util.concurrent.*
 
 internal class ServerDiscovery {
     private val maxAddress = 50
-    private val executor = Executors.newFixedThreadPool(maxAddress)
+    private val executor = Executors.newFixedThreadPool(maxAddress, object : ThreadFactory {
+        var id = 0
+        override fun newThread(r: Runnable?) = Thread(r, "Server-Discovery-TCP-${id++}")
+    })
 
     suspend fun discover(broadcastAddress: InetAddress?): Server? = withContext(Dispatchers.IO) {
         val discoveredServer = Collections.synchronizedSet(HashSet<Server>())
         val futures: MutableList<CompletableFuture<Server?>> = ArrayList()
+
         for (i in 1 until maxAddress) {
             // this is for emulator sessions,
             // as localhost udp server cannot seem to receive upd packets send from emulator
-            futures.add(CompletableFuture.supplyAsync({ discoverLocalNetworkServerPerTcp(i) }, executor))
+            futures.add(CompletableFuture.supplyAsync({
+                return@supplyAsync try {
+                    println("${Thread.currentThread()} Started TCP Discovery of $i")
+                    discoverLocalNetworkServerPerTcp(i)
+                } finally {
+                    println("${Thread.currentThread()} Finished TCP Discovery of $i")
+                }
+            }, executor))
         }
 
         var server: Server? = null
 
+        val udpExecutor = Executors.newSingleThreadExecutor { Thread(it, "Server-Discovery-UDP") }
         try {
-            val future = CompletableFuture.runAsync { discoverLocalNetworkServerPerUdp(broadcastAddress, discoveredServer) }
+            val future = CompletableFuture.runAsync({
+                println("${Thread.currentThread()} Started UDP Discovery")
+                discoverLocalNetworkServerPerUdp(broadcastAddress, discoveredServer)
+            }, udpExecutor)
             try {
                 future[5, TimeUnit.SECONDS]
             } catch (ignored: TimeoutException) {
@@ -49,51 +64,52 @@ internal class ServerDiscovery {
                     e.printStackTrace()
                 }
             }
-            for (discovered in discoveredServer) {
-                if (discovered.isLocal && discovered.isDevServer == isDev) {
-                    server = discovered
-                    break
-                }
-            }
-            if (server == null && !isDev) {
-                server = executor.submit(Callable { discoverInternetServerPerUdp() })[2, TimeUnit.SECONDS]
-            }
+
+            println("${Thread.currentThread()} Total discovered Server: ${discoveredServer.size}")
+            server = discoveredServer.find { it.isLocal && it.isDevServer == isDev }
         } catch (e: Exception) {
             e.printStackTrace()
         }
         executor.shutdownNow()
+        udpExecutor.shutdownNow()
+        println("${Thread.currentThread()} Discovered Server: $server")
         return@withContext server
     }
 
     private fun discoverLocalNetworkServerPerTcp(local: Int): Server? {
         val ipv4 = "192.168.1.$local"
-        val server = Server(ipv4, 3000, true, true)
+        val server = Server(
+            ipv4,
+            3000,
+            isLocal = true,
+            isDevServer = true
+        )
+
         if (!server.isReachable) {
             return null
         }
         try {
             val gson = GsonBuilder()
-                    .registerTypeHierarchyAdapter(DateTime::class.java, DateTimeAdapter())
-                    .create()
+                .registerTypeHierarchyAdapter(DateTime::class.java, DateTimeAdapter())
+                .create()
+
             val client = OkHttpClient.Builder()
-                    .readTimeout(20, TimeUnit.SECONDS)
-                    .build()
+                .readTimeout(20, TimeUnit.SECONDS)
+                .build()
+
             val retrofit = Retrofit.Builder()
-                    .baseUrl(server.address)
-                    .client(client)
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build()
+                .baseUrl(server.address)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+
             val apiImpl = retrofit.create(BasicApi::class.java)
             val devResponse = apiImpl.checkDev("api").execute()
-            val isDev = devResponse.body() != null && devResponse.body()!!
+            val isDev = devResponse.body() ?: return null
+
             return Server(ipv4, 3000, true, isDev)
         } catch (ignored: IOException) {
         }
-        return null
-    }
-
-    private fun discoverInternetServerPerUdp(): Server? {
-        // TODO: 27.07.2019 check if internet server is reachable
         return null
     }
 
@@ -103,7 +119,10 @@ internal class ServerDiscovery {
  * Network discovery using UDP Broadcast (Java)
 ](https://michieldemey.be/blog/network-discovery-using-udp-broadcast/) *
      */
-    private fun discoverLocalNetworkServerPerUdp(broadcastAddress: InetAddress?, discoveredServer: MutableSet<Server>) {
+    private fun discoverLocalNetworkServerPerUdp(
+        broadcastAddress: InetAddress?,
+        discoveredServer: MutableSet<Server>,
+    ) {
         // Find the server using UDP broadcast
         //Open a random port to send the package
         try {
@@ -114,10 +133,19 @@ internal class ServerDiscovery {
                 //Try the some 'normal' ip addresses first
                 try {
                     sendUDPPacket(c, sendData, udpServerPort, InetAddress.getLocalHost())
-                    sendUDPPacket(c, sendData, udpServerPort, InetAddress.getByName("255.255.255.255"))
-                    sendUDPPacket(c, sendData, udpServerPort, InetAddress.getByName("192.168.255.255"))
+                    sendUDPPacket(c,
+                        sendData,
+                        udpServerPort,
+                        InetAddress.getByName("255.255.255.255"))
+                    sendUDPPacket(c,
+                        sendData,
+                        udpServerPort,
+                        InetAddress.getByName("192.168.255.255"))
                     for (i in 1..49) {
-                        sendUDPPacket(c, sendData, udpServerPort, InetAddress.getByName("192.168.1.$i"))
+                        sendUDPPacket(c,
+                            sendData,
+                            udpServerPort,
+                            InetAddress.getByName("192.168.1.$i"))
                     }
                     if (broadcastAddress != null) {
                         sendUDPPacket(c, sendData, udpServerPort, broadcastAddress)
@@ -127,7 +155,8 @@ internal class ServerDiscovery {
                 }
 
                 // Broadcast the message over all the network interfaces
-                val interfaces: List<NetworkInterface> = Collections.list(NetworkInterface.getNetworkInterfaces())
+                val interfaces: List<NetworkInterface> =
+                    Collections.list(NetworkInterface.getNetworkInterfaces())
                 for (networkInterface in interfaces) {
                     if (!networkInterface.isUp) {
                         continue  // Don't want to broadcast to the loopback interface
@@ -158,10 +187,20 @@ internal class ServerDiscovery {
                     // either the node server does not send correctly
                     // or java client does not receive correctly
                     if ("ENTERPRISE_DEV" == message) {
-                        val server = Server(receivePacket.address.hostAddress, 3000, true, true)
+                        val server = Server(
+                            receivePacket.address.hostAddress,
+                            3000,
+                            isLocal = true,
+                            isDevServer = true
+                        )
                         discoveredServer.add(server)
                     } else if ("ENTERPRISE_PROD" == message) {
-                        val server = Server(receivePacket.address.hostAddress, 3000, true, false)
+                        val server = Server(
+                            receivePacket.address.hostAddress,
+                            3000,
+                            isLocal = true,
+                            isDevServer = false
+                        )
                         discoveredServer.add(server)
                     }
                 }
