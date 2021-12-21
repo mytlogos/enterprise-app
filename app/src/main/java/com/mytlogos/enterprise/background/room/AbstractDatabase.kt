1d43ec8945107f204bf162829156dd16dcb7fcb0
mytlogos <mytlogos@hotmail.com>
2021-12-21 17:17:06 +0100

    refactor: use string templates instead of concatenations

--- app/src/main/java/com/mytlogos/enterprise/background/room/AbstractDatabase.kt
+++ app/src/main/java/com/mytlogos/enterprise/background/room/AbstractDatabase.kt
@@ -1,7 +1,10 @@
 package com.mytlogos.enterprise.background.room
 
 import android.content.Context
-import androidx.room.*
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.mytlogos.enterprise.background.room.model.*
@@ -16,7 +19,7 @@ import com.mytlogos.enterprise.background.room.model.RoomMediaList.MediaListMedi
         RoomToDownload::class, RoomMediumInWait::class, RoomDanglingMedium::class,
         RoomFailedEpisode::class, RoomNotification::class, RoomMediumProgress::class,
         RoomMediumPart::class, RoomPartEpisode::class, RoomEditEvent::class, RoomToc::class
-   ],
+    ],
     version = 20
 )
 @TypeConverters(Converters::class)
@@ -48,12 +51,12 @@ abstract class AbstractDatabase : RoomDatabase() {
                 synchronized(AbstractDatabase::class.java) {
                     if (INSTANCE == null || !INSTANCE!!.isOpen) {
                         INSTANCE = Room
-                                .databaseBuilder(
-                                        context.applicationContext,
-                                        AbstractDatabase::class.java,
-                                        DB_Name)
-                                .addMigrations(*migrations())
-                                .build()
+                            .databaseBuilder(
+                                context.applicationContext,
+                                AbstractDatabase::class.java,
+                                DB_Name)
+                            .addMigrations(*migrations())
+                            .build()
                     }
                 }
             }
@@ -62,132 +65,132 @@ abstract class AbstractDatabase : RoomDatabase() {
 
         private fun migrations(): Array<Migration> {
             return arrayOf(
-                    object : Migration(7, 8) {
-                        override fun migrate(database: SupportSQLiteDatabase) {
-                            database.execSQL(
-                                    "CREATE TABLE RoomFailedEpisode (" +
-                                            "episodeId INTEGER NOT NULL, " +
-                                            "failCount INTEGER NOT NULL, " +
-                                            "PRIMARY KEY(episodeId), " +
-                                            "FOREIGN KEY(`episodeId`) REFERENCES `RoomEpisode`(`episodeId`) ON UPDATE NO ACTION ON DELETE CASCADE" +
-                                            ")"
-                            )
-                            database.execSQL(
-                                    "CREATE TABLE RoomNotification (" +
-                                            "title TEXT NOT NULL, " +
-                                            "description TEXT NOT NULL, " +
-                                            "dateTime TEXT NOT NULL, " +
-                                            "PRIMARY KEY(title, dateTime)" +
-                                            ")"
-                            )
-                        }
-                    },
-                    object : Migration(8, 9) {
-                        override fun migrate(database: SupportSQLiteDatabase) {
-                            database.execSQL("ALTER TABLE RoomRelease ADD COLUMN locked INTEGER NOT NULL DEFAULT 0")
-                        }
-                    },
-                    object : Migration(9, 10) {
-                        override fun migrate(database: SupportSQLiteDatabase) {
-                            database.execSQL("ALTER TABLE RoomEpisode ADD COLUMN combiIndex REAL NOT NULL DEFAULT 0")
-                            database.execSQL("UPDATE RoomEpisode SET combiIndex=CAST((totalIndex || \".\" || COALESCE(partialIndex,0)) as decimal)")
-                            database.execSQL("ALTER TABLE RoomPart ADD COLUMN combiIndex REAL NOT NULL DEFAULT 0")
-                            database.execSQL("UPDATE RoomPart SET combiIndex=CAST((totalIndex || \".\" || COALESCE(partialIndex,0)) as decimal)")
-                        }
-                    },
-                    object : Migration(10, 11) {
-                        override fun migrate(database: SupportSQLiteDatabase) {
-                            database.execSQL("CREATE TABLE IF NOT EXISTS RoomMediumProgress " +
-                                    "(mediumId INTEGER NOT NULL, currentReadIndex REAL NOT NULL, " +
-                                    "PRIMARY KEY(`mediumId`), " +
-                                    "FOREIGN KEY(`mediumId`) REFERENCES `RoomMedium`(`mediumId`) " +
-                                    "ON UPDATE NO ACTION ON DELETE CASCADE )"
-                            )
-                            database.execSQL(
-                                    "CREATE INDEX index_RoomMediumProgress_mediumId " +
-                                            "ON RoomMediumProgress (mediumId);"
-                            )
-                            database.execSQL(
-                                    "CREATE INDEX index_RoomMediumProgress_currentReadIndex " +
-                                            "ON RoomMediumProgress (currentReadIndex);"
-                            )
-                            database.execSQL(
-                                    "INSERT OR IGNORE INTO RoomMediumProgress " +
-                                            "SELECT RoomMedium.mediumId, MAX(RoomEpisode.combiIndex) FROM RoomMedium " +
-                                            "INNER JOIN RoomPart ON RoomMedium.mediumId=RoomPart.mediumId " +
-                                            "INNER JOIN RoomEpisode ON RoomPart.partId=RoomEpisode.partId " +
-                                            "WHERE progress = 1"
-                            )
-                        }
-                    },
-                    object : Migration(11, 12) {
-                        override fun migrate(database: SupportSQLiteDatabase) {
-                            database.execSQL("CREATE TABLE IF NOT EXISTS RoomMediumPart " +
-                                    "(mediumId INTEGER NOT NULL, partId INTEGER NOT NULL, " +
-                                    "PRIMARY KEY(`mediumId`, `partId`), " +
-                                    "FOREIGN KEY(`mediumId`) REFERENCES `RoomMedium`(`mediumId`) " +
-                                    "ON UPDATE NO ACTION ON DELETE CASCADE, " +
-                                    "FOREIGN KEY(`partId`) REFERENCES `RoomPart`(`partId`) " +
-                                    "ON UPDATE NO ACTION ON DELETE CASCADE)"
-                            )
-                            database.execSQL("CREATE TABLE IF NOT EXISTS RoomPartEpisode " +
-                                    "(episodeId INTEGER NOT NULL, partId INTEGER NOT NULL, " +
-                                    "PRIMARY KEY(`episodeId`, `partId`), " +
-                                    "FOREIGN KEY(`episodeId`) REFERENCES `RoomEpisode`(`episodeId`) " +
-                                    "ON UPDATE NO ACTION ON DELETE CASCADE, " +
-                                    "FOREIGN KEY(`partId`) REFERENCES `RoomPart`(`partId`) " +
-                                    "ON UPDATE NO ACTION ON DELETE CASCADE)"
-                            )
-                            database.execSQL(
-                                    "CREATE INDEX index_RoomMediumPart_mediumId " +
-                                            "ON RoomMediumPart (mediumId);"
-                            )
-                            database.execSQL(
-                                    "CREATE INDEX index_RoomMediumPart_partId " +
-                                            "ON RoomMediumPart (partId);"
-                            )
-                            database.execSQL(
-                                    "CREATE INDEX index_RoomPartEpisode_episodeId " +
-                                            "ON RoomPartEpisode (episodeId);"
-                            )
-                            database.execSQL(
-                                    "CREATE INDEX index_RoomPartEpisode_partId " +
-                                            "ON RoomPartEpisode (partId);"
-                            )
-                        }
-                    },
-                    object : Migration(12, 13) {
-                        override fun migrate(database: SupportSQLiteDatabase) {
-                            database.execSQL("CREATE TABLE IF NOT EXISTS RoomEditEvent " +
-                                    "(" +
-                                    "id INTEGER NOT NULL, objectType INTEGER NOT NULL, " +
-                                    "eventType INTEGER NOT NULL, dateTime TEXT NOT NULL, " +
-                                    "firstValue TEXT, secondValue TEXT," +
-                                    "PRIMARY KEY(`id`, `objectType`, `eventType`, `dateTime`)" +
-                                    ") "
-                            )
-                        }
-                    },
-                    object : Migration(13, 14) {
-                        override fun migrate(database: SupportSQLiteDatabase) {
-                            database.execSQL("CREATE TABLE IF NOT EXISTS RoomToc " +
-                                    "(" +
-                                    "mediumId INTEGER NOT NULL, link TEXT NOT NULL, " +
-                                    "PRIMARY KEY(`mediumId`, `link`), " +
-                                    "FOREIGN KEY(`mediumId`) REFERENCES `RoomMedium`(`mediumId`) " +
-                                    "ON UPDATE NO ACTION ON DELETE CASCADE " +
-                                    ") "
-                            )
-                            database.execSQL("CREATE INDEX index_RoomToc_mediumId ON RoomToc (mediumId);")
-                            database.execSQL("CREATE INDEX index_RoomToc_link ON RoomToc (link);")
-                        }
-                    },
-                    object : Migration(19, 20) {
-                        override fun migrate(database: SupportSQLiteDatabase) {
-                            database.execSQL("CREATE INDEX index_RoomRelease_releaseDate ON RoomRelease (releaseDate);")
-                            database.execSQL("CREATE INDEX index_RoomEpisode_combiIndex ON RoomEpisode (combiIndex);")
-                        }
-                    },
+                object : Migration(7, 8) {
+                    override fun migrate(database: SupportSQLiteDatabase) {
+                        database.execSQL(
+                            """CREATE TABLE RoomFailedEpisode (
+                                episodeId INTEGER NOT NULL,
+                                failCount INTEGER NOT NULL,
+                                PRIMARY KEY(episodeId),
+                                FOREIGN KEY(`episodeId`) REFERENCES `RoomEpisode`(`episodeId`) ON UPDATE NO ACTION ON DELETE CASCADE
+                                )"""
+                        )
+                        database.execSQL(
+                            """CREATE TABLE RoomNotification (
+                                title TEXT NOT NULL,
+                                description TEXT NOT NULL,
+                                dateTime TEXT NOT NULL,
+                                PRIMARY KEY(title, dateTime)
+                                )"""
+                        )
+                    }
+                },
+                object : Migration(8, 9) {
+                    override fun migrate(database: SupportSQLiteDatabase) {
+                        database.execSQL("ALTER TABLE RoomRelease ADD COLUMN locked INTEGER NOT NULL DEFAULT 0")
+                    }
+                },
+                object : Migration(9, 10) {
+                    override fun migrate(database: SupportSQLiteDatabase) {
+                        database.execSQL("ALTER TABLE RoomEpisode ADD COLUMN combiIndex REAL NOT NULL DEFAULT 0")
+                        database.execSQL("UPDATE RoomEpisode SET combiIndex=CAST((totalIndex || \".\" || COALESCE(partialIndex,0)) as decimal)")
+                        database.execSQL("ALTER TABLE RoomPart ADD COLUMN combiIndex REAL NOT NULL DEFAULT 0")
+                        database.execSQL("UPDATE RoomPart SET combiIndex=CAST((totalIndex || \".\" || COALESCE(partialIndex,0)) as decimal)")
+                    }
+                },
+                object : Migration(10, 11) {
+                    override fun migrate(database: SupportSQLiteDatabase) {
+                        database.execSQL("""
+                                CREATE TABLE IF NOT EXISTS RoomMediumProgress
+                                (mediumId INTEGER NOT NULL, currentReadIndex REAL NOT NULL,
+                                PRIMARY KEY(`mediumId`),
+                                FOREIGN KEY(`mediumId`) REFERENCES `RoomMedium`(`mediumId`)
+                                ON UPDATE NO ACTION ON DELETE CASCADE)
+                                """
+                        )
+                        database.execSQL(
+                            "CREATE INDEX index_RoomMediumProgress_mediumId ON RoomMediumProgress (mediumId);"
+                        )
+                        database.execSQL(
+                            """CREATE INDEX index_RoomMediumProgress_currentReadIndex ON RoomMediumProgress (currentReadIndex);"""
+                        )
+                        database.execSQL("""
+                                INSERT OR IGNORE INTO RoomMediumProgress 
+                                SELECT RoomMedium.mediumId, MAX(RoomEpisode.combiIndex)
+                                FROM RoomMedium
+                                INNER JOIN RoomPart ON RoomMedium.mediumId=RoomPart.mediumId
+                                INNER JOIN RoomEpisode ON RoomPart.partId=RoomEpisode.partId
+                                WHERE progress = 1
+                                """
+                        )
+                    }
+                },
+                object : Migration(11, 12) {
+                    override fun migrate(database: SupportSQLiteDatabase) {
+                        database.execSQL("""CREATE TABLE IF NOT EXISTS RoomMediumPart 
+                                    (mediumId INTEGER NOT NULL, partId INTEGER NOT NULL,
+                                    PRIMARY KEY(`mediumId`, `partId`),
+                                    FOREIGN KEY(`mediumId`) REFERENCES `RoomMedium`(`mediumId`)
+                                    ON UPDATE NO ACTION ON DELETE CASCADE,
+                                    FOREIGN KEY(`partId`) REFERENCES `RoomPart`(`partId`)
+                                    ON UPDATE NO ACTION ON DELETE CASCADE)"""
+                        )
+                        database.execSQL("""CREATE TABLE IF NOT EXISTS RoomPartEpisode 
+                                    (episodeId INTEGER NOT NULL, partId INTEGER NOT NULL,
+                                    PRIMARY KEY(`episodeId`, `partId`),
+                                    FOREIGN KEY(`episodeId`) REFERENCES `RoomEpisode`(`episodeId`)
+                                    ON UPDATE NO ACTION ON DELETE CASCADE,
+                                    FOREIGN KEY(`partId`) REFERENCES `RoomPart`(`partId`)
+                                    ON UPDATE NO ACTION ON DELETE CASCADE)"""
+                        )
+                        database.execSQL(
+                            """CREATE INDEX index_RoomMediumPart_mediumId ON RoomMediumPart (mediumId);"""
+                        )
+                        database.execSQL(
+                            """CREATE INDEX index_RoomMediumPart_partId ON RoomMediumPart (partId);"""
+                        )
+                        database.execSQL(
+                            """CREATE INDEX index_RoomPartEpisode_episodeId ON RoomPartEpisode (episodeId);"""
+                        )
+                        database.execSQL(
+                            """CREATE INDEX index_RoomPartEpisode_partId ON RoomPartEpisode (partId);"""
+                        )
+                    }
+                },
+                object : Migration(12, 13) {
+                    override fun migrate(database: SupportSQLiteDatabase) {
+                        database.execSQL("""
+                            CREATE TABLE IF NOT EXISTS RoomEditEvent 
+                            (
+                            id INTEGER NOT NULL, objectType INTEGER NOT NULL,
+                            eventType INTEGER NOT NULL, dateTime TEXT NOT NULL,
+                            firstValue TEXT, secondValue TEXT,
+                            PRIMARY KEY(`id`, `objectType`, `eventType`, `dateTime`)
+                            )"""
+                        )
+                    }
+                },
+                object : Migration(13, 14) {
+                    override fun migrate(database: SupportSQLiteDatabase) {
+                        database.execSQL("""
+                            CREATE TABLE IF NOT EXISTS RoomToc 
+                            (
+                            mediumId INTEGER NOT NULL, link TEXT NOT NULL,
+                            PRIMARY KEY(`mediumId`, `link`),
+                            FOREIGN KEY(`mediumId`) REFERENCES `RoomMedium`(`mediumId`)
+                            ON UPDATE NO ACTION ON DELETE CASCADE
+                            )"""
+                        )
+                        database.execSQL("CREATE INDEX index_RoomToc_mediumId ON RoomToc (mediumId);")
+                        database.execSQL("CREATE INDEX index_RoomToc_link ON RoomToc (link);")
+                    }
+                },
+                object : Migration(19, 20) {
+                    override fun migrate(database: SupportSQLiteDatabase) {
+                        database.execSQL("CREATE INDEX index_RoomRelease_releaseDate ON RoomRelease (releaseDate);")
+                        database.execSQL("CREATE INDEX index_RoomEpisode_combiIndex ON RoomEpisode (combiIndex);")
+                    }
+                },
             )
         }
     }
